dbb4dd2f6b1941bf2c67f2e2faf05da3a15ab9f5
Thomas <ThomasWT@users.noreply.github.com>
2023-02-22 17:38:59 +0000

    decks

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+const getList = async (page, username) => {
+    let userDecksUrl = await axios.get(`https://tappedout.net/users/${username}/mtg-decks${page ? `?&p=${page}&page=${page}` : ''}`);
+    return cheerio.load(userDecksUrl.data);
+}
+
+const getData = async (username) => {
+  let decks = []
+  let userDecksUrl = await axios.get(`https://tappedout.net/users/${username}/mtg-decks`);
+  let allDecks = cheerio.load(userDecksUrl.data);
+
+  for(var i = 0; i< allDecks(".pagination:nth-child(1) li").length; i++) {
+      let list = await getList(i, username);
+      list(".name a").each((index, link) => {
+              console.log(list(".contents > h5:nth-child(1) > a"));
+        decks.push({
+          link: link.attribs.href,
+          name: list(".contents .deck-wide-header:nth-child(1) > a")[index].text,
+        });
+      })
+
+
+  }
+  console.log(decks)
+}
 
 const getCards = async (username) => {
   let decks = []
   // Get the data
+  const userDecksUrl = await axios.get(`https://tappedout.net/users/${username}/mtg-decks`);
+  const allDecks = cheerio.load(userDecksUrl.data);
+
   const targetUrl = "https://tappedout.net/users/" + username;
   const pageResponse = await axios.get(targetUrl);
 
@@ -28,14 +55,21 @@ const getCards = async (username) => {
               deckDetails[i].cards.push({
                 quantity: parseInt(link.attribs["data-qty"]),
                 name: link.attribs["data-name"],
+                image: deck(".boardlist").eq(i).find(".member > span > a")[0]
+                  .attribs["data-image"],
                 link:
                   "https://tappedout.net/mtg-card/" + link.attribs["data-slug"],
                 description: "",
+                price: deck(".boardlist").eq(i).find(".member > span > a")[0]
+                  .attribs["data-tcg-price"],
               });
             });
+          
         }
 
-        console.log(JSON.stringify(deckDetails))
+      /*   console.log(JSON.stringify(deckDetails)) */
 };
 
-getCards("eothica")
+/* getCards("Licecolony"); */
+
+getData("Licecolony")
