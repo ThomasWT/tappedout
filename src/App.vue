bdce60d0837efe296e072f46b99529d5811c2192
Thomas Thomsen <twt@outlook.dk>
2023-02-23 00:15:36 +0100

    vue setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ decks }}
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data() {
+    return {
+      decks: []
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      let res = await axios.get('https://thomaswt.dk/api/tappedout/doomberg');
+      this.decks = res.data
+    }
+  }
+}
+</script>
