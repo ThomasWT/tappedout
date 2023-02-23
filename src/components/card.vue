8bb674003aebe135821472ee810be83929f04602
Thomas Thomsen <twt@outlook.dk>
2023-02-23 16:42:20 +0100

    icons

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="modal-overlay z-70 relative">
-    <div class="modal mt-2 max-h-[80vh] flex items-center justify-start p-8 flex-col overflow-scroll relative">
+    <div class="modal mt-2 max-h-[80vh] flex items-center justify-start p-8 flex-col overflow-scroll relative w-full">
         <button @click="$emit('close')" class="absolute right-4 top-2 font-bold text-blue-500">Close</button>
       <div class="w-[80%]">
         <img class="rounded-md shadow-md" :src="card.image" />
       </div>
-      <div>
+      <div class="w-full">
         <p class="font-bold text-xl mt-2">{{ card.name }}</p>
-
-        <p class="mb-4 text-left" v-html="desc"></p>
+        <div class="bg-gray-200 p-2 w-full">    <p class="mb-4 text-left text-sm" v-html="desc"></p></div>
+    
         <p class="mb-2">Current price (TCG): ${{ card.price  }}</p>
         <a class="text-blue-500" target="_blank" :href="card.link">See on Tappedout</a>
       </div>
