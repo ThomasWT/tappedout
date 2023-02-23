fa539faeafed1cfd908207e807151c2b011a6fec
Thomas <ThomasWT@users.noreply.github.com>
2023-02-23 14:25:29 +0000

    new stuff

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="modal-overlay z-70 relative">
-    <div class="modal h-[80vh] flex items-center justify-start p-8 flex-col overflow-scroll relative">
+    <div class="modal mt-2 max-h-[80vh] flex items-center justify-start p-8 flex-col overflow-scroll relative">
         <button @click="$emit('close')" class="absolute right-4 top-2 font-bold text-blue-500">Close</button>
       <div class="w-[80%]">
-        <img :src="card.image" />
+        <img class="rounded-md shadow-md" :src="card.image" />
       </div>
       <div>
         <p class="font-bold text-xl mt-2">{{ card.name }}</p>
 
-        <p class="mb-4" v-html="desc"></p>
+        <p class="mb-4 text-left" v-html="desc"></p>
         <p class="mb-2">Current price (TCG): ${{ card.price  }}</p>
         <a class="text-blue-500" target="_blank" :href="card.link">See on Tappedout</a>
       </div>
@@ -45,12 +45,13 @@ import axios from 'redaxios'
   display: flex;
   justify-content: center;
   background-color: #000000da;
+  height:calc(100% - 80px);
 }
 
 .modal {
+  display:flex;
   text-align: center;
   background-color: white;
-  height: 85vh;
   width: 90%;
   border-radius: 20px;
 }
